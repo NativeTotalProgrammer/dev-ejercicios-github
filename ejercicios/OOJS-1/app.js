4c4ba2303f659d4ed056a75ec2cbdf3c465858fb
Luis Alberto Villar Ortiz <lu.villarortiz@gmail.com>
2023-10-01 00:58:43 -0600

    ejercicio 1 completo

--- ejercicios/OOJS-1/app.js
+++ ejercicios/OOJS-1/app.js
@@ -1,7 +1,18 @@
 class Shape {
+  constructor(_name, _sides, _sideLength) {
+    this.name       = _name;
+    this.sides      = _sides;
+    this.sideLength = _sideLength;
+  };
 
-  name;
-  sides;
-  sideLength;
+  get calcPerimeter() {
+    const perimeter = this.sides * this.sideLength;
+    return `The perimeter of the ${this.name} is ${perimeter}`;
+  };
+};
 
-}
+const square = new Shape("square", 4, 5);
+console.log(square.calcPerimeter);
+
+const triangle = new Shape("triangle", 3, 3);
+console.log(triangle.calcPerimeter);
